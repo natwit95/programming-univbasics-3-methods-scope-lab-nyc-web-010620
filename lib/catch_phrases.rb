7bec41686ea61cac5c55681dff1621f41d27bff3
Natalia  Wit <natwit95@gmail.com>
2019-12-18 05:57:49 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,11 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
-end
-
-def toadstool
-  puts status
-end
+# def mario
+#   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+#   puts phrase
+# end
+#
+# def toadstool
+#   puts status
+# end
 
 def link
   puts "It's Dangerous To Go Alone! Take This."
