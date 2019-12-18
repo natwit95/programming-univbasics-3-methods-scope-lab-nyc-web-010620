5ae5cc3eee94d961a835c0d0a17806b00afac1cc
Natalia  Wit <natwit95@gmail.com>
2019-12-18 06:48:50 +0000

    Done.

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
-describe "mario" do
+
+def mario = "phrase" do
   it "puts out 'It's-a me, Mario!'" do
+    phrase = "It's-a me, Mario!"
     expect{mario}.to output("It's-a me, Mario!\n").to_stdout
   end
 end
+
+mario (phrase)
 
 describe "toadstool" do
   it "puts out 'Thank You Mario! But Our Princess Is In Another Castle!'" do
