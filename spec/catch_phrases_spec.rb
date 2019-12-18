8d1cd54d240668eb1d0e65835e40f7be2837d6fb
Natalia  Wit <natwit95@gmail.com>
2019-12-18 06:39:17 +0000

    Automatically backed up by Learn

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 
-describe mario (phrase = "It's-a me, Mario!") do
+describe mario (phrase) do
   it "puts out 'It's-a me, Mario!'" do
     phrase = "It's-a me, Mario!"
     expect{mario}.to output("It's-a me, Mario!\n").to_stdout
